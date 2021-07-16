20ecfc7d309d76c665431a453a410b422c768632
Kavya-349 <83696676+Kavya-349@users.noreply.github.com>
2021-07-16 19:56:07 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var canvas =-document.getElementById("my_canvas");
-var ctx = canvas.getContext("2d");
-var color = blue;
-ctx.beginPath();
-ctx.strokeStyle = color;
-ctx.lineWidth = 2;
-ctx.rect(150, 143, 430, 200);
-ctx.stroke();
