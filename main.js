f1daed9987bb0b3fa89f0783220d38bdcc255be4
Kavya-349 <83696676+Kavya-349@users.noreply.github.com>
2021-07-23 19:22:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,30 +9,30 @@ ctx.stroke();
 
 ctx.beginPath(); 
 ctx.strokeStyle = "blue"; 
-ctx.lineWidth = 4; 
+ctx.lineWidth = 6; 
 ctx.arc(250, 200, 40 ,0 , 2*Math.PI); 
 ctx.stroke();
 
 ctx.beginPath(); 
 ctx.strokeStyle = "black"; 
-ctx.lineWidth = 4; 
+ctx.lineWidth = 6; 
 ctx.arc(350, 200, 40 ,0 , 2*Math.PI); 
 ctx.stroke();
 
 ctx.beginPath(); 
 ctx.strokeStyle = "red"; 
-ctx.lineWidth = 4; 
+ctx.lineWidth = 6; 
 ctx.arc(450, 200, 40 ,0 , 2*Math.PI); 
 ctx.stroke();
 
 ctx.beginPath(); 
 ctx.strokeStyle = "yellow"; 
-ctx.lineWidth = 4; 
+ctx.lineWidth = 6; 
 ctx.arc(300, 240, 40 ,0 , 2*Math.PI); 
 ctx.stroke();
 
 ctx.beginPath(); 
 ctx.strokeStyle = "green"; 
-ctx.lineWidth = 4; 
+ctx.lineWidth = 6; 
 ctx.arc(400, 240, 40 ,0 , 2*Math.PI); 
 ctx.stroke();
